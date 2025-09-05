65f8d6e0c6a980a96d0f431b7da2c41b14da4560
su-metal <tossy104104@gmail.com>
2025-09-05 16:15:46 +0900

    test(api): favorites ping (is function alive?)

--- web/api/favorites.js
+++ web/api/favorites.js
@@ -3,9 +3,6 @@ export const runtime = 'edge';
 export default async function handler(req) {
   return new Response(JSON.stringify({ ok: true, ping: 'favorites' }), {
     status: 200,
-    headers: {
-      'content-type': 'application/json; charset=utf-8',
-      'x-handler': 'favorites-ping'
-    }
+    headers: { 'content-type': 'application/json; charset=utf-8' },
   });
 }
