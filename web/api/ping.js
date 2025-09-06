282aa62e0a1b16fcac6926248852c736d0c00902
su-metal <tossy104104@gmail.com>
2025-09-06 15:11:34 +0900

    feat: zero-start minimal (root=web) + ESM /api/health

--- web/api/ping.js
+++
@@ -1,5 +0,0 @@
-module.exports = (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('content-type', 'application/json; charset=utf-8');
-  res.end(JSON.stringify({ ok: true, pong: true }));
-};
